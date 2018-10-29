ec8c9ec0000e83c1060d4f747c3f79e78e9c7c52
billy <lijun526757124@163.com>
2018-10-30 00:32:56 +0800

    update url

--- config/url.js
+++ config/url.js
@@ -1,6 +1,6 @@
 import env from './env'
 
-const DEV_URL = 'http://localhost:10001/'
-const PRO_URL = 'http://localhost:10001/'
+const DEV_URL = 'http://118.31.32.168:10001/'
+const PRO_URL = 'http://118.31.32.168:10001/'
 
 export default env === 'development' ? DEV_URL : PRO_URL
